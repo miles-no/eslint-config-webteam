8a274ffcc1b8c31812f1e0fb6ec3673e57eb8ef9
wenche <wenche.tollevsen@gmail.com>
2017-01-18 10:04:53 +0100

    jsx extensions on

--- rules/imports.js
+++ rules/imports.js
@@ -114,8 +114,7 @@ module.exports = {
     // Ensure consistent use of file extension within the import path
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
     'import/extensions': ['error', 'always', {
-      js: 'never',
-      jsx: 'never',
+      js: 'never'
     }],
 
     // Enforce a convention in module import order
