64f908a8be0c44e747ca7321bab8ec27b6011bfa
mskutle <magne.skutle@gmail.com>
2018-11-20 15:48:22 +0100

    fix: removed deprecated option

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ module.exports = {
   },
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2017,
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true //enable support for the experimental object rest/spread properties 
-    }
+    ecmaVersion: 2018,
   },
   extends: [
     './rules/core.js',
